c1a593ed3480e62f422a7868179985b91dfdf31c
chigozie emmanuel <mmajoy55@gmail.com>
2023-11-30 03:06:52 +0100

    '#change animation'

--- js/script.js
+++ js/script.js
@@ -1,100 +1,108 @@
 // for image slider/
 $(document).ready(function () {
-    $("#owl-carousel").owlCarousel({
-      items: 1,
-      loop: true,
-      dots: true,
-      nav: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      smartSpeed: 900,
-      autoplayHoverPause: true,
-      touchDrag  : true,
-      mouseDrag  : true,
+  $("#owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 900,
+    autoplayHoverPause: true,
+    touchDrag: true,
+    mouseDrag: true,
+    onInitialized: startAnimation,
+    onTranslated: startAnimation,
 
-  
-    });
+
   });
-  // the end/
-  window.addEventListener('scroll', function () {
-    var header = document.querySelector('header');
-    var scrolled = window.scrollY > 0;
-    header.classList.toggle('scrolled', scrolled);
-  });
-
-  $(document).ready(function () {
-    $("#little-sliders").owlCarousel({
-      items: 1,
-      loop: true,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 10000,
-      smartSpeed: 200,
-      autoplayHoverPause: true,
-      touchDrag  : true,
-      mouseDrag  : true,
-
+});
+function startAnimation() {
+  // Add and remove the 'animate__animated' class to restart the animation
+  $('.owl-item.active h1, .owl-item.active p').removeClass('animate__animated').css('opacity', '0');
+  $('.owl-item.active h1, .owl-item.active p').addClass('animate__animated').css('opacity', '1');
   
-    });
+}
+// the end/
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('header');
+  var scrolled = window.scrollY > 0;
+  header.classList.toggle('scrolled', scrolled);
+});
+
+$(document).ready(function () {
+  $("#little-sliders").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    smartSpeed: 200,
+    autoplayHoverPause: true,
+    touchDrag: true,
+    mouseDrag: true,
+
+
   });
-  
-  $(document).ready(function () {
-    $("#mobile-view").owlCarousel({
-      items: 1,
-      loop: true,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 10000,
-      smartSpeed: 200,
-      autoplayHoverPause: true,
-      touchDrag  : true,
-      mouseDrag  : true,
+});
 
-  
-    });
+$(document).ready(function () {
+  $("#mobile-view").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    smartSpeed: 200,
+    autoplayHoverPause: true,
+    touchDrag: true,
+    mouseDrag: true,
+
+
   });
+});
 
 
 
 
-  $(document).ready(function () {
-    $("#quote-slides").owlCarousel({
-      items: 1,
-      loop: true,
-      dots: false,
-      autoplay: true,
-      autoplayTimeout: false,
-      smartSpeed: false,
-      autoplayHoverPause: true,
-      touchDrag  : true,
-      mouseDrag  : true,
+$(document).ready(function () {
+  $("#quote-slides").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: false,
+    smartSpeed: false,
+    autoplayHoverPause: true,
+    touchDrag: true,
+    mouseDrag: true,
 
-  
-    });
+
   });
- 
+});
 
-  $(document).ready(function () {
-    $("#little-quote-slides").owlCarousel({
-      items: 1,
-      loop: true,
-      dots: false,
-      autoplay: true,
-      autoplayTimeout: false,
-      smartSpeed: false,
-      autoplayHoverPause: true,
-      touchDrag  : true,
-      mouseDrag  : true,
 
-  
-    });
+$(document).ready(function () {
+  $("#little-quote-slides").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: false,
+    smartSpeed: false,
+    autoplayHoverPause: true,
+    touchDrag: true,
+    mouseDrag: true,
+
+
   });
+});
 
-  const nav = document.querySelector("#mobile-nav");
-  const click = document.querySelector(".ham");
+const nav = document.querySelector("#mobile-nav");
+const click = document.querySelector(".ham");
 const cliker = document.querySelector(".fa-solid fa-bars fa-2xl");
-click.addEventListener('click', () =>{
-    nav.style.display = "block";
+click.addEventListener('click', () => {
+  nav.style.display = "block";
 });
 function closeNav() {
   document.querySelector("#mobile-nav").style.display = "none";
@@ -102,6 +110,7 @@ function closeNav() {
 
 document.addEventListener("DOMContentLoaded", function () {
   const collapsibles = document.querySelectorAll(".collapsible");
+  const collable = document.querySelector(".content-collasible");
 
   for (const collapsible of collapsibles) {
     collapsible.addEventListener("click", function () {
@@ -115,6 +124,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
       this.classList.toggle("active");
       content.style.display = "block";
+      collapsible.addEventListener("onclick", () => {
+              
+      })
     });
   }
 });
@@ -123,12 +135,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-function changeTab(){
+function changeTab() {
   const changetab1 = document.querySelector("#tab-1");
   const changetab2 = document.querySelector("#tab-2");
   const btn1 = document.querySelector("#Year");
   const btn2 = document.querySelector("#month");
-  btn1.addEventListener("click", ()=>{
+  btn1.addEventListener("click", () => {
     changetab1.style.display = "none";
     changetab2.style.display = "flex"
     btn1.style.backgroundColor = "rgb(16, 76, 186)";
@@ -136,7 +148,7 @@ function changeTab(){
     btn2.style.backgroundColor = "white";
     btn2.style.color = "black";
   });
-  btn2.addEventListener("click", ()=> {
+  btn2.addEventListener("click", () => {
     changetab1.style.display = "flex";
     changetab2.style.display = "none";
     btn1.style.backgroundColor = "white";
